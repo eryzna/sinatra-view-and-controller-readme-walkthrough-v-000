58f79028fb17b10258515cd6a72c725d08b8a64f
Elana Ryznar <eryzna@gmail.com>
2019-08-09 00:56:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   get '/reverse' do
     original_string = params["string"]
-    reversed_string = original_string.reverse
+    @reversed_string = original_string.reverse
     erb :reverse
   end
 
